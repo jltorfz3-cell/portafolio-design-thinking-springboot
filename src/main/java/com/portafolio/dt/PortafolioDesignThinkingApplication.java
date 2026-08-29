71a5a04a4db39899a198815156b6d046444e7712
jltorfz3-cell <jltorfz3@gmail.com>
2026-08-28 20:47:40 -0500

    w

--- src/main/java/com/portafolio/dt/PortafolioDesignThinkingApplication.java
+++ src/main/java/com/portafolio/dt/PortafolioDesignThinkingApplication.java
@@ -1,4 +1,23 @@
 package com.portafolio.dt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication public class PortafolioDesignThinkingApplication { public static void main(String[] args){SpringApplication.run(PortafolioDesignThinkingApplication.class,args);} }
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@SpringBootApplication public class PortafolioDesignThinkingApplication { 
+
+    public static void main(String[] args){
+
+        BCryptPasswordEncoder encoder =
+                new BCryptPasswordEncoder();
+
+        System.out.println("Admin123     = " +
+                encoder.encode("Admin123"));
+
+        System.out.println("Profesor123  = " +
+                encoder.encode("Profesor123"));
+
+        System.out.println("Estudiante123 = " +
+                encoder.encode("Estudiante123"));
+
+        SpringApplication.run(PortafolioDesignThinkingApplication.class,args);
+    } 
+}
