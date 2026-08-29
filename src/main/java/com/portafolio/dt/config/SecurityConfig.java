8ded8acc92a14f1f9132da8fbc1ba9895c9a1493
jltorfz3-cell <jltorfz3@gmail.com>
2026-08-28 19:29:41 -0500

    Imports

--- src/main/java/com/portafolio/dt/config/SecurityConfig.java
+++ src/main/java/com/portafolio/dt/config/SecurityConfig.java
@@ -1,15 +1,9 @@
 package com.portafolio.dt.config;
 
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 import com.portafolio.dt.security.JwtFilter; 
 import org.springframework.context.annotation.*; 
@@ -21,8 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder; 
 import org.springframework.security.web.*; 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-
 @Configuration 
 public class SecurityConfig { 
     @Bean PasswordEncoder 
